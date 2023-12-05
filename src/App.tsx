f816e0474db363baaef8fff8ac6798af4974989d
cemalgnlts <cemalgnlts@gmail.com>
2023-12-05 18:07:53 +0300

    monaco-vscode-api integration work.

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+import { init, attachPanels } from "./libs/editor/init";
+
+import { Parts } from "vscode/vscode/vs/workbench/services/layout/browser/layoutService";
+
+function App() {
+  const [ready, setReady] = useState<boolean>(false);
+  const editorRef = useRef<HTMLDivElement>(null);
+  const explorerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const setup = async () => {
+      await init();
+      setReady(true);
+    };
+
+    if (!ready) setup();
+  }, []);
+
+  useEffect(() => {
+    if (!editorRef.current || !explorerRef.current || !ready) return;
+
+    attachPanels([
+      {
+        panel: Parts.EDITOR_PART,
+        element: editorRef.current
+      },
+      {
+        panel: Parts.SIDEBAR_PART,
+        element: explorerRef.current
+      }
+    ]);
+  }, [ready]);
+
+  return (
+    <>
+      <div ref={editorRef}></div>
+      <div ref={explorerRef}></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/libs/editor/init.ts
@@ -0,0 +1,30 @@
+import { initialize as initializeServices } from "vscode/services";
+
+import getDialogsServiceOverride from "@codingame/monaco-vscode-dialogs-service-override";
+import getStorageServiceOverrride from "@codingame/monaco-vscode-storage-service-override";
+import getViewsServiceOverride, {
+  Parts,
+  attachPart
+} from "@codingame/monaco-vscode-views-service-override";
+
+import getModelServiceOverride from "@codingame/monaco-vscode-model-service-override";
+
+type Panels = Array<{
+  panel: Parts;
+  element: HTMLDivElement;
+}>;
+
+export async function init() {
+  await initializeServices({
+    ...getStorageServiceOverrride(),
+    ...getViewsServiceOverride(),
+    ...getModelServiceOverride(),
+    ...getDialogsServiceOverride()
+  });
+}
+
+export function attachPanels(panels: Panels) {
+  for (const { panel, element } of panels) {
+    attachPart(panel, element);
+  }
+}
